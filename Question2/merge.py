1ccbbe2fb0fc6241497c1d3460bfca303c3c47ba
atubak400 <atubak40@gmail.com>
2023-11-24 22:26:38 +0000

    code explanation added

--- Question2/merge.py
+++ Question2/merge.py
@@ -21,3 +21,22 @@ with pd.ExcelWriter('test2.xlsx', engine='xlsxwriter') as writer:
     # Write the combined DataFrame to a single sheet
     df_combined.to_excel(writer, sheet_name='CombinedSheet', index=False)
 
+
+
+"""
+-> pd.to_datetime(df_sheet1['Transaction Date']): This converts the 'Transaction Date' column of df_sheet1 into a pandas datetime object.
+
+-> The .dt.strftime('%-m/%-d/%Y') method in your code indeed formats the datetime objects to only display the date in the specified format, 
+effectively eliminating the time component in the displayed output.
+
+-> .strftime('%-m/%-d/%Y'): This is used to format the datetime object into a specific string format. 
+The format string '%-m/%-d/%Y' specifies that the date should be formatted as:
+%-m: Month as a decimal number without a leading zero.
+%-d: Day of the month as a decimal number without a leading zero.
+%Y: Year with century as a decimal number.
+For example, a date like "2023-01-05" would be formatted as "1/5/2023".
+
+-> The axis=1 parameter in the pd.concat() function is used to specify the axis along which the concatenation should happen. 
+In pandas, axis=0 is the default and represents concatenation along the index (rows), while axis=1 represents concatenation along the columns.
+
+"""
